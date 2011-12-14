e953fc89edc31b16889a7d50eb19ae49e2f41488
mu-ad-dib <colindeeb@gmail.com>
2011-12-14 15:09:19 -0800

    Update todos/apps/todos/todos.js

--- todos/apps/todos/todos.js
+++ todos/apps/todos/todos.js
@@ -22,3 +22,11 @@ Todos.todoListController = SC.ArrayController.create({
   // Initialize the array controller with an empty array.
   content: []
 });
+
+ // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  }
+});
