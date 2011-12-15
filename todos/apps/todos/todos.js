acba63876eafb53069235b21e355aad1292980ba
Colin Deeb <colindeeb@gmail.com>
2011-12-14 17:57:57 -0800

    adding 'mark all as done' button and reflexive behavior to list

--- todos/apps/todos/todos.js
+++ todos/apps/todos/todos.js
@@ -6,9 +6,9 @@
 
 Todos = SC.Application.create();
 
-Todos.Todo = SC.Object.extend({
-  title: null,
-  isDone: false
+Todos.Todo = SC.Object.extend({ 
+ title: null,
+ isDone: false
 });
 
 Todos.CreateTodoView = SC.TextField.extend({
@@ -36,25 +36,40 @@ Todos.StatsView = SC.TemplateView.extend({
   }.property('remaining')
 });
 
-SC.ready(function() {
-  Todos.mainPane = SC.TemplatePane.append({
-    layerId: 'todos',
-    templateName: 'todos'
-  });
-});
-
 Todos.todoListController = SC.ArrayController.create({
   // Initialize the array controller with an empty array.
   content: [],
 
  // Creates a new todo with the passed title, then adds it
   // to the array.
+
   createTodo: function(title) {
     var todo = Todos.Todo.create({ title: title });
     this.pushObject(todo);
-  }
+  },
 
   remaining: function() {
     return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this);
+  },
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+ 
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
   }.property('@each.isDone')
+});
+
+SC.ready(function() {
+  Todos.mainPane = SC.TemplatePane.append({
+    layerId: 'todos',
+    templateName: 'todos'
+  });
 });
